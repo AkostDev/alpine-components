720a4497d0e9aeaa0be1f5035d23a6178ae15d12
devil <i@akost.dev>
2023-09-22 16:27:35 +0500

    feat: Install plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,20 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {},
+  },
+  daisyui: {
+    themes: ['light', 'dark', 'emerald'],
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/container-queries'),
+    require('daisyui')
   ],
 }
 
